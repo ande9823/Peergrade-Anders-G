3d1f06cce2d3b5a3ce1ad26793082bf81de958b8
Anders Gaarsdal <angaa19@student.sdu.dk>
2020-02-15 18:59:52 +0100

    Addition worst code

---
+++ worst code addition/worst code addition/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace worst_code_addition
+{
+    class Program
+    {
+        static void Main(string[] args) {Console.Write("Please write an integer."); Console.WriteLine(); string stringNumber1 = Console.ReadLine(); int number1 = Convert.ToInt32(stringNumber1); Console.Write("Please write another integer."); Console.WriteLine(); string stringNumber2 = Console.ReadLine(); int number2 = Convert.ToInt32(stringNumber2); int result; if (number1 < number2 || number1 > number2) result = number1 + number2; else result = (number1 + number2) * 3; Console.Write(number1 + " " + "+" + " " + number2 + " " + "=" + " " + result); Console.ReadLine();}
+    }
+}
