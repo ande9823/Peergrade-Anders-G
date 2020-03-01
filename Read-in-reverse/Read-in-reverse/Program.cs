0ac098f020caafaf088a00c58a813e00b8089fac
Anders Gaarsdal <angaa19@student.sdu.dk>
2020-03-01 22:05:06 +0100

    Added a new PeerGrade with stack shown in reverse

---
+++ Read-in-reverse/Read-in-reverse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read_in_reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack();
+            string line; 
+            Console.WriteLine("Enter one or more lines of text (press CTRL+Z to exit the input):");
+            Console.WriteLine("Then press enter to show reverse stack");
+
+            do 
+            {
+                line = Console.ReadLine();
+                if (line != null)
+                {
+                    myStack.Push(line);
+                }
+            } while (line != null);
+
+            while (myStack.Count > 0)
+                Console.WriteLine(myStack.Pop());
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
